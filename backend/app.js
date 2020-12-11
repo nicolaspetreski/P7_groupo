798f7769ffeb575d20f8b2e05d2627592d64419b
nicolaspetreski <n.petreski@live.com>
2020-12-11 01:35:00 -0800

    creation app

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const helmet = require("helmet");
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+const app = express();
+const path = require('path');
+
+//Importation routes
+const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
+
+
+//CORS error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+
+module.exports = app;
