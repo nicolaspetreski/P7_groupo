9cd5665fc6489f17f4e732d61e995cc9e6594d27
nicolaspetreski <n.petreski@live.com>
2020-12-22 06:20:05 -0800

    add seq sync to srv

--- backend/server.js
+++ backend/server.js
@@ -39,11 +39,15 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
+const db = require('./models');
+db.sequelize.sync()
+	.then(()=>{
+		server.on('listening', () => {
+			const address = server.address();
+			const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+			console.log('Listening on ' + bind);
+		})
+	})
+	.catch(err => console.log(err))
 server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
-
 server.listen(port);
