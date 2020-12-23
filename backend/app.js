659879bc20c1014676fd33c26048324172416064
nicolaspetreski <n.petreski@live.com>
2020-12-23 03:05:29 -0800

    revue app.js

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,38 @@
 const express = require('express');
-const helmet = require("helmet");
-const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
-
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { expressShield } = require('node-shield');
+const helmet = require('helmet');
+const usersRoutes = require('./routes/users');
+const postsRoutes = require('./routes/posts');
+const reportPostsRoutes = require('./routes/reportPosts');
+const reportCommentsRoutes = require('./routes/reportComments');
+const commentsRoutes = require('./routes/comments');
+const researchRoutes = require('./routes/research');
+const issueRoutes = require('./routes/issueUsers');
 const path = require('path');
 
-//Importation routes
-const postRoutes = require('./routes/post');
-const userRoutes = require('./routes/user');
-
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
 app.use(helmet());
-
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
+app.use(expressShield({
+	errorHandler: (shieldError, req, res, next) => {
+		console.error(shieldError);
+		res.sendStatus(400);
+	}
+}));
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/files', express.static(path.join(__dirname, 'files')));
+
+app.use('/api/posts', postsRoutes);
+app.use('/api/auth', usersRoutes);
+app.use('/api/report', reportPostsRoutes);
+app.use('/api/report/comment', reportCommentsRoutes);
+app.use('/api/posts', commentsRoutes);
+app.use('/api/research', researchRoutes);
+app.use('/api/issue', issueRoutes);
 
 module.exports = app;
